23321f05e9cc0c289e596cd563af6dc411d23185
Fernando <fernando2256@hotmail.com>
2023-05-17 20:31:48 -0300

    Nota com While e IF/Else IF para aprovado e reprovado

--- src/Nota.java
+++ src/Nota.java
@@ -10,20 +10,20 @@ até que o usuário informe um valor válido.
 
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);// interação com usuário digitando
 
-        double nota;
+        double nota; // aceita numeros quebrados
 
         System.out.println("Insira uma nota de Zero a 10");
-        nota = scan.nextDouble();
+        nota = scan.nextDouble();// interação com usuário
 
-        while (nota < 0 | nota > 10) {
+        while (nota < 0 | nota > 10) { // loop de repetição em caso de nao satisfazer as condições
             System.out.println("Valor Invalido");
             System.out.println("Digite um valor Valido");
-            nota = scan.nextDouble();
+            nota = scan.nextDouble(); // interação novamente
 
         }
-        if (nota<=5){
+        if (nota<=5){ // condição de aprovado ou reprovado
             System.out.println("Reprovado");
         }else if (nota>=6){
             System.out.println("Aprovado");
